9e2b539f909cc4a290ddfd2e7b1057d0ca0c3836
Viktor Tomkovič <viktor.tomkovic@gmail.com>
2024-03-23 20:18:19 +0100

    Selected teammate is updating on selecting other teammate
    Yes, I am reloading the whole document and I like it.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"io"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -68,20 +72,21 @@ func newTeammates() []Teammate {
 }
 
 type Page struct {
-	Teammates []Teammate
+	Teammates    []Teammate
 	SelectedName string
-	ActivatedBy string
+	ActivatedBy  string
 }
 
 func newPage() Page {
 	return Page{
-		Teammates: newTeammates(),
+		Teammates:    newTeammates(),
 		SelectedName: "",
-		ActivatedBy: "",
+		ActivatedBy:  "",
 	}
 }
 
 func main() {
+	fmt.Println("Daily Automation. Welcome and have a nice productive day.")
 	server := echo.New()
 	server.Use(middleware.Logger())
 	page := newPage()
@@ -93,8 +98,40 @@ func main() {
 		activatedBy := c.QueryParam("activatedBy")
 		page.SelectedName = selectedName
 		page.ActivatedBy = activatedBy
+		numberSelected, _ := strconv.Atoi(c.QueryParam("numberSelected"))
+		if numberSelected > 0 {
+			// We moved from editing one teammate to another. Update recently edited one.
+			modifiedTeammate, err := createTeammate(
+				c.QueryParam("name"),
+				c.QueryParam("done"),
+				c.QueryParam("willDo"),
+				c.QueryParam("blockers"),
+				c.QueryParam("generalRemarks"))
+			if err == nil {
+				i := slices.IndexFunc(page.Teammates, func(t Teammate) bool {
+					return t.Name == modifiedTeammate.Name
+				})
+				if i >= 0 {
+					page.Teammates[i] = modifiedTeammate
+				}
+			}
+		}
 		return c.Render(200, "index", page)
 	})
 	err := server.Start(":12121")
 	server.Logger.Fatal(err)
+	fmt.Println("Daily Automation. Bye and have a nice productive day.")
+}
+
+func createTeammate(name string, done string, willDo string, blockers string, generalRemarks string) (Teammate, error) {
+	if name == "" {
+		return Teammate{}, errors.New("Empty name")
+	}
+	return Teammate{
+		Name:           name,
+		Done:           done,
+		WillDo:         willDo,
+		Blockers:       blockers,
+		GeneralRemarks: generalRemarks,
+	}, nil
 }
