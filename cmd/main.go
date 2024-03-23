b8b44dfc1e417e50e5617fbe3150818defd62826
Viktor Tomkovič <viktor.tomkovic@gmail.com>
2024-03-23 23:01:05 +0100

    Saving to harddrive implemented

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"io"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
@@ -52,7 +54,7 @@ func (t Teammate) SelectedClass(selectedName string) string {
 
 type Teammates = []Teammate
 
-func newTeammates() []Teammate {
+func newDefaultTeammates() []Teammate {
 	return []Teammate{
 		{
 			Name:           "Viktor",
@@ -71,6 +73,24 @@ func newTeammates() []Teammate {
 	}
 }
 
+func newTeammate(name string) Teammate {
+	return Teammate{
+		Name: name,
+		Done: "",
+		WillDo: "",
+		Blockers: "",
+		GeneralRemarks: "",
+	}
+}
+
+func newNamedTeammates(names []string) []Teammate {
+	result := []Teammate{}
+	for _, name := range names {
+		result = append(result, newTeammate(name))
+	}
+	return result
+}
+
 type Page struct {
 	Teammates    []Teammate
 	SelectedName string
@@ -79,17 +99,105 @@ type Page struct {
 
 func newPage() Page {
 	return Page{
-		Teammates:    newTeammates(),
+		Teammates:    newDefaultTeammates(),
 		SelectedName: "",
 		ActivatedBy:  "",
 	}
 }
 
+func createTeammate(name string, done string, willDo string, blockers string, generalRemarks string) (Teammate, error) {
+	if name == "" {
+		return Teammate{}, errors.New("Empty name")
+	}
+	return Teammate{
+		Name:           name,
+		Done:           done,
+		WillDo:         willDo,
+		Blockers:       blockers,
+		GeneralRemarks: generalRemarks,
+	}, nil
+}
+
+type Config struct {
+	Port  int
+	Names []string
+}
+
+func CreateDeafultConfig() Config {
+	return Config{
+		Port:  12121,
+		Names: []string{"John Blow", "Johan Brahms", "Ján Hraško"},
+	}
+}
+
+func initConfig() Config {
+	result := CreateDeafultConfig()
+	if _, err := os.Stat(".da.json"); err == nil {
+		// exists
+		fileContent, err := os.ReadFile(".da.json")
+		if err == nil {
+			var config Config
+			err := json.Unmarshal(fileContent, &config)
+			if err != nil {
+				fmt.Println("Error during unmarshalling json from config file .da.json. Using default configuration.")
+			} else {
+				result = config
+			}
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		// path does *not* exist
+		fmt.Println("File .da.json does not exists. Using default configuration.")
+	} else {
+		// File cannot be reached. See err for details.
+		fmt.Println("File .da.json cannot be reached. Using default configuration.")
+	}
+	return result
+}
+
+const TeammatesFilename = "daily.json"
+
+func writeTeammates(teammates []Teammate) {
+	content, err := json.MarshalIndent(teammates, "", "\t")
+	if err != nil {
+		fmt.Println("CANNOT MARSHAL daily.json!")
+		fmt.Println(err)
+	} else {
+		err = os.WriteFile(TeammatesFilename, content, 0644)
+		if err != nil {
+			fmt.Println("CANNOT WRITE daily.json!")
+			fmt.Println(err)
+		}
+	}
+}
+
+func readTeammates() ([]Teammate, error) {
+	content, err := os.ReadFile(TeammatesFilename)
+	if err != nil {
+		return []Teammate{}, err
+	}
+	var teammates []Teammate
+	err = json.Unmarshal(content, &teammates)
+	if err != nil {
+		return []Teammate{}, err
+	}
+	return teammates, nil
+}
+
 func main() {
 	fmt.Println("Daily Automation. Welcome and have a nice productive day.")
+	config := initConfig()
+	teammatesInit, err := readTeammates()
+	page := newPage()
+	if err == nil {
+		page.Teammates = teammatesInit
+	} else {
+		page.Teammates = newNamedTeammates(config.Names)
+	}
+	fmt.Println(config.Names)
+	fmt.Println(teammatesInit)
+	fmt.Println(page.Teammates)
 	server := echo.New()
 	server.Use(middleware.Logger())
-	page := newPage()
 	server.File("/main.css", "css/main.css")
 	server.File("/favicon.png", "favicon.png")
 	server.Renderer = newTemplate()
@@ -98,6 +206,13 @@ func main() {
 		activatedBy := c.QueryParam("activatedBy")
 		page.SelectedName = selectedName
 		page.ActivatedBy = activatedBy
+		teammatesFromFile, err := readTeammates()
+		if err != nil {
+			fmt.Println("Could not read teammates from daily.json. Using default teammates.")
+			fmt.Println(err)
+		} else {
+			page.Teammates = teammatesFromFile
+		}
 		numberSelected, _ := strconv.Atoi(c.QueryParam("numberSelected"))
 		if numberSelected > 0 {
 			// We moved from editing one teammate to another. Update recently edited one.
@@ -113,25 +228,17 @@ func main() {
 				})
 				if i >= 0 {
 					page.Teammates[i] = modifiedTeammate
+				} else {
+					page.Teammates = append(page.Teammates, modifiedTeammate)
 				}
 			}
+			// Write down changes to harddrive
+			cloneTeammates := page.Teammates
+			writeTeammates(cloneTeammates)
 		}
 		return c.Render(200, "index", page)
 	})
-	err := server.Start(":12121")
+	err = server.Start(fmt.Sprintf(":%d", config.Port))
 	server.Logger.Fatal(err)
 	fmt.Println("Daily Automation. Bye and have a nice productive day.")
-}
-
-func createTeammate(name string, done string, willDo string, blockers string, generalRemarks string) (Teammate, error) {
-	if name == "" {
-		return Teammate{}, errors.New("Empty name")
-	}
-	return Teammate{
-		Name:           name,
-		Done:           done,
-		WillDo:         willDo,
-		Blockers:       blockers,
-		GeneralRemarks: generalRemarks,
-	}, nil
 }
